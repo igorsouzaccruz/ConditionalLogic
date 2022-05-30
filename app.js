c8a63ee8969b938f47c57b4dd104c9093f8a5bd7
igorsouzaccruz <igorsouzaccruz@gmail.com>
2022-05-29 23:50:17 -0300

    operacional, tabela verdade completa não implementada

--- app.js
+++ app.js
@@ -1,269 +1,245 @@
-function insertAt(array, index, ...elements){
+function insertAt(array, index, ...elements) {
     array.splice(index, 0, ...elements);
 }
 
-function vetorToString(input){
-    var output =''
-    for(var i = 0; i<input.length; i++){
+function vetorToString(input) {
+    var output = ''
+    for (var i = 0; i < input.length; i++) {
         var output = output + input[i];
     }
     return output;
 }
 
-function tratandoNot(expr){
-    for(var i = 0; i < expr.length; i++){
-        if(expr[i] == "~" && (expr[i+1] == "F" || expr[i+1] == "V"))
-        {
-            if(expr[i+1] == "F")
-            {
-                expr[i+1] = "V";
+function tratandoNot(expressao) {
+    for (var i = 0; i < expressao.length; i++) {
+        if (expressao[i] == "~" && (expressao[i + 1] == "F" || expressao[i + 1] == "V")) {
+            if (expressao[i + 1] == "F") {
+                expressao[i + 1] = "V";
             }
-            else if(expr[i+1] == "V")
-            {
-                expr[i+1] = "F";
+            else if (expressao[i + 1] == "V") {
+                expressao[i + 1] = "F";
             }
             //Remover o elemento i      
-            expr.splice(i,1);
+            expressao.splice(i, 1);
         }
     }
-    return expr;
+    return expressao;
 }
 
-function verificaNOT(expr){
+function verificaNOT(expressao) {
 
     var verificador = 0;
 
-    for(var i = 0; i <expr.length; i++){
-        if(expr[i] == "~" && (expr[i+1] == "V" || expr[i+1] == "F")){
+    for (var i = 0; i < expressao.length; i++) {
+        if (expressao[i] == "~" && (expressao[i + 1] == "V" || expressao[i + 1] == "F")) {
             verificador = 1;
         }
     }
     return verificador;
 }
 
-function avaliar(expr){
+function avaliar(expressao) {
     var i;
     var log;
 
     //Primeiro na ordem de precedencia
-    do{
-        expr = tratandoNot(expr);
+    do {
+        expressao = tratandoNot(expressao);
 
-    } while(verificaNOT(expr) == 1)
-   
+    } while (verificaNOT(expressao) == 1)
+
 
     //Segundo na ordem de precedencia
-    for(i = 0; i< expr.length; i++){
-        if(expr[i] == "^"){
-            if((expr[i-1] == "V" || expr[i-1] == "F") && (expr[i+1] == "V" || expr[i+1] == "F")){
-                log = expr[i-1] + expr[i+1];
+    for (i = 0; i < expressao.length; i++) {
+        if (expressao[i] == "^") {
+            if ((expressao[i - 1] == "V" || expressao[i - 1] == "F") && (expressao[i + 1] == "V" || expressao[i + 1] == "F")) {
+                log = expressao[i - 1] + expressao[i + 1];
                 log = vetorToString(log);
-                expr.splice(i,1);
-                if(log == "VV")
-                {
+                expressao.splice(i, 1);
+                if (log == "VV") {
                     resposta = "V";
                 }
-                else
-                {
+                else {
                     resposta = "F";
                 }
-                expr.splice(i,1);
-                expr.splice(i-1,1);
-                insertAt(expr, i-1, resposta)
+                expressao.splice(i, 1);
+                expressao.splice(i - 1, 1);
+                insertAt(expressao, i - 1, resposta)
             }
         }
     }
 
     //Segundo na ordem de precedencia
-    for(i = 0; i < expr.length; i++){
-        if(expr[i] == "v"){
-            if((expr[i-1] == "V" || expr[i-1] == "F") && (expr[i+1] == "V" || expr[i+1] == "F")){
-                log = expr[i-1] + expr[i+1];
+    for (i = 0; i < expressao.length; i++) {
+        if (expressao[i] == "v") {
+            if ((expressao[i - 1] == "V" || expressao[i - 1] == "F") && (expressao[i + 1] == "V" || expressao[i + 1] == "F")) {
+                log = expressao[i - 1] + expressao[i + 1];
                 log = vetorToString(log);
-                expr.splice(i,1);
-                if(log == "FF")
-                {
+                expressao.splice(i, 1);
+                if (log == "FF") {
                     resposta = "F";
                 }
-                else
-                {
+                else {
                     resposta = "V";
                 }
-                expr.splice(i,1);
-                expr.splice(i-1,1);
-                insertAt(expr, i-1, resposta)
+                expressao.splice(i, 1);
+                expressao.splice(i - 1, 1);
+                insertAt(expressao, i - 1, resposta)
             }
         }
     }
     //Terceira ba irden de orecencia
-    for(i = 0; i < expr.length; i++){
-        if(expr[i] == "→"){
-            if((expr[i-1] == "V" || expr[i-1] == "F") && (expr[i+1] == "V" || expr[i+1] == "F")){
-                log = expr[i-1] + expr[i+1];
+    for (i = 0; i < expressao.length; i++) {
+        if (expressao[i] == "→") {
+            if ((expressao[i - 1] == "V" || expressao[i - 1] == "F") && (expressao[i + 1] == "V" || expressao[i + 1] == "F")) {
+                log = expressao[i - 1] + expressao[i + 1];
                 log = vetorToString(log);
-                expr.splice(i,1);
-                if(log == "VF")
-                {
+                expressao.splice(i, 1);
+                if (log == "VF") {
                     resposta = "F";
                 }
-                else
-                {
+                else {
                     resposta = "V";
                 }
-                expr.splice(i,1);
-                expr.splice(i-1,1);
-                insertAt(expr, i-1, resposta)
+                expressao.splice(i, 1);
+                expressao.splice(i - 1, 1);
+                insertAt(expressao, i - 1, resposta)
             }
         }
     }
     //Quarto na ordem de precedencia
-    for(i = 0; i < expr.length; i++){
-        if(expr[i] == "⟷"){
-            if((expr[i-1] == "V" || expr[i-1] == "F") && (expr[i+1] == "V" || expr[i+1] == "F")){
-                log = expr[i-1] + expr[i+1];
+    for (i = 0; i < expressao.length; i++) {
+        if (expressao[i] == "⟷") {
+            if ((expressao[i - 1] == "V" || expressao[i - 1] == "F") && (expressao[i + 1] == "V" || expressao[i + 1] == "F")) {
+                log = expressao[i - 1] + expressao[i + 1];
                 log = vetorToString(log);
-                expr.splice(i,1);
-                if(log == "VV" || log =="FF")
-                {
+                expressao.splice(i, 1);
+                if (log == "VV" || log == "FF") {
                     resposta = "V";
                 }
-                else
-                {
+                else {
                     resposta = "F";
                 }
-                expr.splice(i,1);
-                expr.splice(i-1,1);
-                insertAt(expr, i-1, resposta)
+                expressao.splice(i, 1);
+                expressao.splice(i - 1, 1);
+                insertAt(expressao, i - 1, resposta)
             }
         }
     }
-    for(i = 0; i < expr.length; i++)
-    {
-        if(expr[i] == "(" && expr[i+2] == ")")
-        {
-            expr.splice(i,1)
-            expr.splice(i+1,1)
+    for (i = 0; i < expressao.length; i++) {
+        if (expressao[i] == "(" && expressao[i + 2] == ")") {
+            expressao.splice(i, 1)
+            expressao.splice(i + 1, 1)
         }
     }
-    return expr;
+    return expressao;
 }
 
-function valores(expr, a = "F", b = "F", c = "F"){
-    
-    for(var i = 0; i < expr.length; i++){
-        if(expr[i] == "A"){
-            expr[i] = a;
+function valores(expressao, a = "F", b = "F", c = "F") {
+
+    for (var i = 0; i < expressao.length; i++) {
+        if (expressao[i] == "A") {
+            expressao[i] = a;
         }
-        else if(expr[i] == "B"){
-            expr[i] = b;
+        else if (expressao[i] == "B") {
+            expressao[i] = b;
         }
-        else if(expr[i] == "C"){
-            expr[i] = c;
+        else if (expressao[i] == "C") {
+            expressao[i] = c;
         }
     }
-    return expr;
+    return expressao;
 }
 
-function calcular(){
+function calcular() {
     //Declaração de variáveis
-    var expr = [];
+    var expressao = [];
     var qtd = 0;
     var letras = ["A", "B", "C"];
     var atr = [];
-    var atr1 = [];
-    var atr2= [];
-    
-    //Recebimento da expressão
-    expr =  document.getElementById('equacao').innerHTML.split("");
 
-     //Recebimento de valores lógicos das proposições
+    //Recebimento da expressaoessão
+    expressao = document.getElementById('equacao').innerHTML.split("");
+    var solucao = expressao;
 
-    for(var i = 0; i < letras.length; i++)
-    {
-        if(expr.includes(letras[i]))
-        {
+    //Recebimento de valores lógicos das proposições
+
+    for (var i = 0; i < letras.length; i++) {
+        if (expressao.includes(letras[i])) {
             qtd++;
         }
     }
 
-     var tabela = tabelaVerdade(qtd);
-   
-    
- 
-        for(var i = 0; i< parseInt(Math.pow(2,qtd));i++)
-        {
-            atr[i]= tabela.A[i];
-            atr1[i]= tabela.B[i];
-            atr2[i]= tabela.C[i];
-        }  
+   // var tabela = tabelaVerdade(qtd);
 
-        console.log(expr);
-        expr = valores(expr, a = atr[0], b = "F", c = atr2[0]);
-        console.log(expr);
-        var texto = document.getElementById('resposta').innerHTML = "Solução: "  
-        
-       
+    /* for (var i = 0; i < parseInt(Math.pow(2, qtd)); i++) {        
+        expressao = valores(solucao, a = tabela.A[i], b = tabela.B[i], c = tabela.C[i]); 
+    } */
+    for (var j = 0; j < qtd; j++) {
+        do {
+            atr[j] = prompt("Informe o valor lógico da variável: " + letras[j] + ": ");
+        } while (atr[j] != "V" && atr[j] != "F");
+    }
+    expressao = valores(expressao, a = atr[0], b = atr[1], c = atr[2]);
 
-      if(expr.length == 1)
-        {
-           document.getElementById('resposta').innerHTML = texto + vetorToString(valores(expr)); 
+    var texto = document.getElementById('resposta').innerHTML = "Solução: </br><hr>"
+
+    if (expressao.length == 1) {
+        document.getElementById('resposta').innerHTML = texto + vetorToString(valores(expressao));
+    }
+    else {
+        while (expressao.length > 1) {
+            expressao = avaliar(expressao);
+            console.log("teste");
         }
-        else{
-            while(expr.length > 1){
-                expr = avaliar(expr);
-                console.log("teste");
-            }
-            expr = avaliar(expr);
-            document.getElementById('resposta').innerHTML = vetorToString(expr);
-            //texto = document.getElementById('resposta').innerHTML = texto + valores(expr) +"<hr>"; 
-        }  
-         
-        //document.getElementById('resposta').innerHTML = texto + "</br>"+ atr + "</br>"+ atr1 + "</br>" + atr2;
+        expressao = avaliar(expressao);
+        document.getElementById('resposta').innerHTML = texto + vetorToString(expressao);
+    }
 }
 
-function insert(param){
+function insert(param) {
     let equacao = document.getElementById('equacao').innerHTML;
     document.querySelector('#equacao').innerHTML = equacao + param;
 }
 
-function limpar(){
+function limpar() {
     document.querySelector('#equacao').innerHTML = "";
 }
 
-function back(){
+function back() {
     let equacao = document.getElementById('equacao').innerHTML;
-    document.querySelector('#equacao').innerHTML = equacao.substring(0,equacao.length-1);
+    document.querySelector('#equacao').innerHTML = equacao.substring(0, equacao.length - 1);
 }
 
-function tabelaVerdade(n){
- //cria os valores da tabela verdade para as variáveis A,B,C 
+/* function tabelaVerdade(n) {
+    //cria os valores da tabela verdade para as variáveis A,B,C 
     let tabela = {
         A: [],
         B: [],
         C: []
     }
 
-    var repeat = parseInt(Math.pow(2,n));
-    for(var i = 0; i < repeat;i++){
+    var repeat = parseInt(Math.pow(2, n));
+    for (var i = 0; i < repeat; i++) {
         // Gera valor de A 
-        if(i < (repeat/2) && n >= 1){
+        if (i < (repeat / 2) && n >= 1) {
             tabela.A.push("V");
-        }else{
+        } else {
             tabela.A.push("F");
         }
         // Gera valor de B
-        if(i <(repeat/4) || ((repeat/2) <= i && i < (repeat - repeat/4)) && n >= 2){
+        if (i < (repeat / 4) || ((repeat / 2) <= i && i < (repeat - repeat / 4)) && n >= 2) {
             tabela.B.push("V");
-        }else{
+        } else {
             tabela.B.push("F");
         }
         // Gera valor de C
-        if(i%2 == 0 && n == 3){
+        if (i % 2 == 0 && n == 3) {
             tabela.C.push("V");
-        }else{
+        } else {
             tabela.C.push("F");
         }
     }
 
     return tabela;
-}
+} */
